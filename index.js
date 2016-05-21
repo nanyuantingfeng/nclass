e1804abe31a78b6e799acfd26f2f78154f3238c9
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2016-05-21 18:44:38 +0800

    version 1.6.0

--- index.js
+++ index.js
@@ -4,20 +4,40 @@
  **********************************************/
 
 function Class(Super, init) {
-    var N, NEW;
-    N = function() {};
-    init = init || N;
-    N.prototype = Super.prototype;
-    NEW = function() {return new NEW.fn.initialize(arguments);};
-    NEW.fn = NEW.prototype = new N();
-    NEW.fn.initialize = function(M) {
-        init.apply(this, M);
-        return this;
-    };
-    NEW.fn.constructor = NEW;
-    NEW.fn.initialize.prototype = NEW.fz = NEW.fn;
-    NEW.initialize = init;
-    return NEW;
+  var N, NEW;
+  N = function() {};
+  init = init || N;
+  N.prototype = Super.prototype;
+  NEW = function() {
+    return new NEW.fn.initialize(arguments);
+  };
+  NEW.fn = NEW.prototype = new N();
+  NEW.fn.initialize = function(M) {
+    init.apply(this, M);
+    return this
+  };
+  NEW.fn.constructor = NEW;
+  NEW.fn.initialize.prototype = NEW.fz = NEW.fn;
+  NEW.initialize = init;
+  return NEW;
 }
 
-module.exports = Class.Class = Class;
+function Class2(Super, init) {
+  var N, NEW;
+  N = function() {};
+  init = init || N;
+  N.prototype = Super.prototype;
+  NEW = function() {
+    init.apply(this, arguments);
+    return this;
+  };
+  NEW.fn = NEW.prototype = new N();
+  NEW.fn.constructor = NEW;
+  NEW.initialize = init;
+  return NEW;
+}
+
+module.exports['default'] = Class;
+module.exports.Class = Class;
+module.exports.Class2 = Class2; 
+
